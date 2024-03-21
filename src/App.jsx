1c22a11783f6953d091d13b2e6431fc48142f339
Biser Svilenov Stanev <biser@MacBook-Air-de-Biser.local>
2024-03-21 18:54:54 +0100

    ejercicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,63 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App () {
-  <></>
-};
+function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [pokemonData, setPokemonData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      if (!searchTerm) {
+        setPokemonData(null);
+        return;
+      }
+      setIsLoading(true);
+      try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchTerm.toLowerCase()}`);
+        if (!response.ok) {
+          throw new Error('Pokemon no encontrado');
+        }
+        const data = await response.json();
+        setPokemonData(data);
+        setError(null);
+      } catch (error) {
+        setError(error.message);
+        setPokemonData(null);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchPokemon();
+  }, [searchTerm]);
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  return (
+    <div className="app">
+      <h1>Nombre del Pokemon</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="Nombre del Pokemon"
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+        {isLoading && <p>Loading...</p>}
+      </form>
+      {error && <p>{error}</p>}
+      {pokemonData && (
+        <div className="detalles-pokemon">
+          <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
+          <h2>{pokemonData.name}</h2>
+          {}
+        </div>
+      )}
+    </div>
+  );
+}
 export default App;
